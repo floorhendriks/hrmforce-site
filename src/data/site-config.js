16f93e3b30c1d1292473b0e23d2cb4c0ba7ffff1
floorhendriks <f.hendriks@hrmforce.com>
2026-08-20 12:31:19 +0200

    Create site-config.js

---
+++ src/data/site-config.js
@@ -0,0 +1,5 @@
+// Centrale site-config. Vul FORMSPREE_ENDPOINT in met je eigen Formspree-URL
+// (bijv. "https://formspree.io/f/xxxxxxxx"). Zolang deze leeg is, vallen de
+// formulieren terug op een mailto-link naar service@hrmforce.com.
+export const FORMSPREE_ENDPOINT = "";
+export const CONTACT_EMAIL = "service@hrmforce.com";
