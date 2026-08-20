16954391556025e553f0bde8904f9aa4f2a536e7
floorhendriks <f.hendriks@hrmforce.com>
2026-08-20 16:03:18 +0200

    Update site-config.js

--- src/data/site-config.js
+++ src/data/site-config.js
@@ -1,7 +1,7 @@
 // Centrale site-config. Vul FORMSPREE_ENDPOINT in met je eigen Formspree-URL
 // (bijv. "https://formspree.io/f/xxxxxxxx"). Zolang deze leeg is, vallen de
 // formulieren terug op een mailto-link naar service@hrmforce.com.
-export const FORMSPREE_ENDPOINT = "";
+export const FORMSPREE_ENDPOINT = "https://formspree.io/f/mqpznaqn";
 export const CONTACT_EMAIL = "service@hrmforce.com";
 
 // Shopify Storefront (voor de ingebedde /shop/). Vul STOREFRONT_TOKEN in met een
